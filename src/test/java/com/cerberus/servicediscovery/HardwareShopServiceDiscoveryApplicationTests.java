9036d976e093b587f0b573dd6bd56d48cd408c94
egor <egormalov.2007@gmail.com>
2024-11-04 19:10:44 +1000

    Initial commit

---
+++ src/test/java/com/cerberus/servicediscovery/HardwareShopServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerberus.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareShopServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
